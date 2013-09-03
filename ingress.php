e1e460a84298fe3919a6ba0fc9a939f29977fc0f
ingressresearch <ingressresearch51f0df2957052@mon-mail.org>
2013-09-03 13:24:19 +0200

    Updated for new encoding of JSON packets (sigh!)

--- ingress.php
+++ ingress.php
@@ -4,11 +4,11 @@ include_once("debug.inc");
 include_once("myDB.class");
 include_once("ingress.inc");
 
-$var = curl_download("https://www.ingress.com/rpc/dashboard.getPaginatedPlextsV2");
+$var = curl_download("https://www.ingress.com/r/dashboard.getPaginatedPlextsV2");
 
 if (isset($var["result"])) {
 	$var["result"] = array_reverse($var["result"]);
-	//print_r($var); //die();
+	print_r($var); //die();
 	saveIntelData($var);
 } else {
 	print_r($var);
